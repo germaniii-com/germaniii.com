b947ad57fa10724d03858d2dd52b925acb9e5791
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-09-05 15:18:29 +0800

    Change font to Roboto

--- root/app/layout.tsx
+++ root/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['cyrillic'],
+  weight: '100'
+})
 
 export const metadata: Metadata = {
   title: 'German III Felisarta',
@@ -18,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
