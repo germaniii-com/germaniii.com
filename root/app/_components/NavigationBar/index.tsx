12bf671b3918048b2b565f22e3af14c78030f0ef
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-09-05 16:40:15 +0800

    Add Navbar

---
+++ root/app/_components/NavigationBar/index.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+const navigationButtons = ["Home", "Projects"];
+
+const NavigationBar = () => {
+  return (
+    <nav className="bg-white dark:bg-black">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            className="text-black font-medium text-center p-3 rounded \
+            hover:text-white hover:bg-black \
+            dark:text-white dark:hover:text-black dark:hover:bg-white"
+          >
+            Blog
+          </button>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-black">
+            {navigationButtons.map((button: string) => {
+              return (
+                <li key={2 + button}>
+                  <Link
+                    key={1 + button}
+                    href={`/${button.toLocaleLowerCase()}`}
+                    className="block text-black p-3 rounded \
+                     hover:text-white hover:bg-black \
+                     dark:text-white dark:hover:text-black dark:hover:bg-white "
+                    aria-current="page"
+                  >
+                    {button}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
--- root/app/layout.tsx
+++ root/app/layout.tsx
@@ -1,27 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import NavigationBar from "./_components/NavigationBar";
 
 const roboto = Roboto({
-  subsets: ['cyrillic'],
-  weight: '100'
-})
+  subsets: ["cyrillic"],
+  weight: "100",
+});
 
 export const metadata: Metadata = {
-  title: 'German III Felisarta',
-  description: "I am a software engineer with a diverse skill set in various programming languages and frameworks. \
+  title: "German III Felisarta",
+  description:
+    "I am a software engineer with a diverse skill set in various programming languages and frameworks. \
   I have learned to think fundamentally, becoming language agnostic, and solving problems from its core. \
   From business applications to creative projects, nothing is impossible with my diverse skillset.",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <NavigationBar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
