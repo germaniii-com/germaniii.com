fdb2a239983041126a1f8994e3cce6a08beab304
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-09-16 16:40:05 +0800

    Add Rewrite from root to /home

--- root/next.config.js
+++ root/next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
