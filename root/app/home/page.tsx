b3b33532b905ae806043a95f153816431334f3df
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-10-08 15:14:57 +0800

    Add Top Projects and Section Header Component with Refectoring

--- root/app/home/page.tsx
+++ root/app/home/page.tsx
@@ -1,10 +1,12 @@
 import AboutMe from "./_components/AboutMe";
+import TopProjects from "./_components/TopProjects";
 import styles from "./index.module.scss";
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <AboutMe />
+      <TopProjects />
       <h1>
         Hello, this website is still in development. Please check back later.
       </h1>
