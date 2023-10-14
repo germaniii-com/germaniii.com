a98df98fd5c7d8ba596d7bf6dfe16817da0b157a
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-10-14 13:21:14 +0800

    Add Project Grid

--- root/app/home/_components/TopProjects/index.tsx
+++ root/app/home/_components/TopProjects/index.tsx
@@ -1,4 +1,5 @@
 import SectionHeader from "../SectionHeader";
+import ProjectCard from "./_components/ProjectCard";
 import styles from "./index.module.scss";
 import Image from "next/image";
 
@@ -6,7 +7,15 @@ const TopProjects = () => {
   return (
     <div className={styles.topProjectsWrapper}>
       <SectionHeader header={"PROJECTS"} subHeader={"What I have made"} />
-      <div className={styles.textWrapper}>In Progress...</div>
+      <div className={styles.projectGrid}>
+        <div className={styles.row}>
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+        <div className={styles.row}>
+          <ProjectCard />
+        </div>
+      </div>
     </div>
   );
 };
