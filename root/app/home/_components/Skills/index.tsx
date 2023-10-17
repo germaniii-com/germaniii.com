de8d022f405ca6d88fb25d9d4a72da84d1be603f
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-10-17 14:51:54 +0800

    Add a Skills Section

---
+++ root/app/home/_components/Skills/index.tsx
@@ -0,0 +1,58 @@
+import styles from "./index.module.scss";
+
+import {
+  BiLogoCPlusPlus,
+  BiLogoJava,
+  BiLogoCss3,
+  BiLogoHtml5,
+  BiLogoPhp,
+  BiLogoJavascript,
+  BiLogoTypescript,
+  BiLogoAndroid,
+  BiLogoSass,
+  BiLogoReact,
+  BiLogoRedux,
+  BiLogoSpringBoot,
+  BiLogoGit,
+  BiLogoDocker,
+} from "react-icons/bi";
+
+import {
+  SiMysql,
+  SiLinux,
+  SiArduino,
+  SiGnubash,
+  SiLaravel,
+} from "react-icons/si";
+import SectionHeader from "../SectionHeader";
+
+const Skills = () => {
+  return (
+    <div className={styles.skillsWrapper}>
+      <SectionHeader header="SKILLS" subHeader="What I can do" />
+      <div className={styles.skillGrid}>
+        <BiLogoCPlusPlus />
+        <BiLogoJava />
+        <BiLogoCss3 />
+        <BiLogoHtml5 />
+        <BiLogoPhp />
+        <BiLogoJavascript />
+        <BiLogoTypescript />
+        <BiLogoAndroid />
+        <BiLogoSass />
+        <BiLogoReact />
+        <BiLogoRedux />
+        <BiLogoSpringBoot />
+        <SiLaravel />
+        <BiLogoGit />
+        <BiLogoDocker />
+        <SiMysql />
+        <SiLinux />
+        <SiArduino />
+        <SiGnubash />
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
