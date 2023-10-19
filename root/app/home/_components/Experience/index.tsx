fd09852c4861ba10672920d93b43556a39722038
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-10-19 19:49:52 +0800

    Create Experience Section

---
+++ root/app/home/_components/Experience/index.tsx
@@ -0,0 +1,12 @@
+import SectionHeader from "../SectionHeader";
+import styles from "./index.module.scss";
+
+const Experience = () => {
+  return (
+    <div className={styles.experienceWrapper}>
+      <SectionHeader header="EXPERIENCE" subHeader="My professional history" />
+    </div>
+  );
+};
+
+export default Experience;
