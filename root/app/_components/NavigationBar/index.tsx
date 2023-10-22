c2ab8fa44c507e50f21d6cd9c9ec8ee7fd12c670
germaniiifelisarta <germaniiifelisarta@gmail.com>
2023-10-22 13:42:39 +0800

    Center the Navbar

--- root/app/_components/NavigationBar/index.tsx
+++ root/app/_components/NavigationBar/index.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.scss";
 import { BiSun } from "react-icons/bi";
 import NavButton from "./_components/NavButton";
 
-const navigationButtons: string[] = ["Home", "Projects"];
+const navigationButtons: string[] = ["Home", "Projects", "Blog"];
 
 const NavigationBar = () => {
   return (
@@ -11,11 +11,11 @@ const NavigationBar = () => {
         {navigationButtons.map((button: string) => (
           <NavButton key={button} label={button} />
         ))}
+        <li className={styles.external}>
+          <BiSun />
+        </li>
       </ul>
-      <div className={styles.external}>
-        <BiSun />
-        <span>Blog</span>
-      </div>
+      {/* <div className={styles.external}></div> */}
     </div>
   );
 };
