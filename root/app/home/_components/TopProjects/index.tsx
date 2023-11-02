078a0c41bbe8d4e999ed56d4e9629b6c6056ac28
German III <germaniiifelisarta@gmail.com>
2023-11-02 20:00:02 +0800

    Change the color of third top project

--- root/app/home/_components/TopProjects/index.tsx
+++ root/app/home/_components/TopProjects/index.tsx
@@ -7,34 +7,43 @@ import { DIRECTION } from "@/app/Globals";
 import { Projects as projects } from "@/app/Globals";
 
 const TopProjects = () => {
-  const projectsSlice = projects.slice(0, 2);
+  const projectsSlice = projects.slice(0, 3).map((project, index) => {
+    if (index == 2)
+      project = {
+        ...project,
+        direction: DIRECTION.FLOAT_RIGHT,
+      };
+
+    return project;
+  });
+
   return (
     <div className={styles.topProjectsWrapper}>
       <SectionHeader header={"PROJECTS"} subHeader={"What I have made"} />
       <div className={styles.projectGrid}>
         <div className={styles.row}>
           <ProjectCard
-            title={projects[0].title}
-            subTitle={projects[0].subTitle}
-            description={projects[0].description}
-            direction={projects[0].direction}
-            image={projects[0].image}
+            title={projectsSlice[0].title}
+            subTitle={projectsSlice[0].subTitle}
+            description={projectsSlice[0].description}
+            direction={projectsSlice[0].direction}
+            image={projectsSlice[0].image}
           />
           <ProjectCard
-            title={projects[1].title}
-            subTitle={projects[1].subTitle}
-            description={projects[1].description}
-            direction={projects[1].direction}
-            image={projects[1].image}
+            title={projectsSlice[1].title}
+            subTitle={projectsSlice[1].subTitle}
+            description={projectsSlice[1].description}
+            direction={projectsSlice[1].direction}
+            image={projectsSlice[1].image}
           />
         </div>
         <div className={styles.row}>
           <ProjectCard
-            title={projects[2].title}
-            subTitle={projects[2].subTitle}
-            description={projects[2].description}
-            direction={projects[2].direction}
-            image={projects[2].image}
+            title={projectsSlice[2].title}
+            subTitle={projectsSlice[2].subTitle}
+            description={projectsSlice[2].description}
+            direction={projectsSlice[2].direction}
+            image={projectsSlice[2].image}
           />
           <Link href={"/projects"} replace={true}>
             <p>See more projects </p>
