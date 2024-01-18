f883ae05f63fb72c07b9d9137c89a5b1a49102bf
German III <germaniiifelisarta@gmail.com>
2024-01-18 16:54:08 +0800

    Add middleware

--- root/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import styles from "./index.module.scss";
-
-const RootPage = async () => {
-  redirect("/home");
-  return <div className={styles.emptyBody}></div>;
-};
-
-export default RootPage;
